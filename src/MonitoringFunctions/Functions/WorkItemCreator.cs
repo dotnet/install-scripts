5bbe42e9f61f5c862444d4c3cc92c56854c1f55b
Bekir Ozturk <bozturk@microsoft.com>
2020-08-12 13:59:46 +0200

    Added checks to see if work item already exists.

--- src/MonitoringFunctions/Functions/WorkItemCreator.cs
+++ src/MonitoringFunctions/Functions/WorkItemCreator.cs
@@ -25,6 +25,7 @@ namespace MonitoringFunctions.Functions
     internal static class WorkItemCreator
     {
         private const string ProjectName = "devdiv";
+        private const string IncidentAreaPath = "DevDiv\\NET Tools\\install-scripts-incidents";
         private static readonly string? _devdivAdoPAT = Environment.GetEnvironmentVariable("devdiv-ado-pat");
         private static readonly Uri _devdivCollectionUri = new Uri("https://devdiv.visualstudio.com/DefaultCollection/");
 
@@ -33,61 +34,69 @@ namespace MonitoringFunctions.Functions
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            List<WorkItem> createdWorkItems = new List<WorkItem>();
-
             using StreamReader requestStream = new StreamReader(req.Body);
             string requestBody = await requestStream.ReadToEndAsync().ConfigureAwait(false);
 
-            if(string.IsNullOrWhiteSpace(requestBody))
+            if (string.IsNullOrWhiteSpace(requestBody))
             {
-                return new BadRequestObjectResult("Request body shouldn't be empty");
+                throw new InvalidOperationException("Request body shouldn't be empty");
             }
 
-            AlertNotificationData data;
-            try
+            AlertNotificationData data = JsonConvert.DeserializeObject<AlertNotificationData>(requestBody);
+
+            if (data.State != AlertState.Alerting)
             {
-                data = JsonConvert.DeserializeObject<AlertNotificationData>(requestBody);
+                throw new ArgumentOutOfRangeException("Alert state is not \"Alerting\". This notification shouldn't have been sent to this function.");
             }
-            catch(JsonException e)
+
+            VssCredentials credentials = new VssBasicCredential("", _devdivAdoPAT);
+            WorkItemTrackingHttpClient workItemClient = new WorkItemTrackingHttpClient(_devdivCollectionUri, credentials);
+            ProjectHttpClient projectClient = new ProjectHttpClient(_devdivCollectionUri, credentials);
+            TeamProject devdivProject = await projectClient.GetProject(ProjectName).ConfigureAwait(false);
+
+            if (devdivProject == null)
             {
-                string errorMessage = "Deserialization of the request body has failed.";
-                log.LogError(e, errorMessage);
-                return new BadRequestObjectResult(errorMessage);
+                throw new ProjectDoesNotExistException($"The project \"{ProjectName}\" was not found or you do not have permission to access it.");
             }
 
             string alertMessage = data.Message ?? "Alert triggered";
             int alertingMonitorCount = data.MatchingAlerts?.Length ?? 0;
+            List<WorkItem> createdWorkItems = new List<WorkItem>();
 
-            if(data.State != AlertState.Alerting)
-            {
-                string errorMessage = "Alert state is not \"Alerting\". This notification shouldn't have been sent to this function.";
-                log.LogError(errorMessage);
-                return new BadRequestObjectResult(errorMessage);
-            }
-
-            for(int i=0; i<alertingMonitorCount; i++)
+            for (int i = 0; i < alertingMonitorCount; i++)
             {
                 AlertEvaluation? match = data.MatchingAlerts![i];
 
-                if(match == null || match.Value.Tags == null)
+                if (match == null || match.Value.Tags == null)
                 {
                     log.LogError($"Evaluationg match #{i} has invalid data.");
                     continue;
                 }
 
-                if(!match.Value.Tags.ContainsKey("monitor_name"))
+                if (!match.Value.Tags.ContainsKey("monitor_name"))
                 {
                     log.LogError($"Evaluation match #{i} doesn't specify a monitor name which is required.");
                     continue;
                 }
 
-                string title = $"{match.Value.Tags["monitor_name"]} {alertMessage}";
+                string title = $"#{match.Value.Tags["monitor_name"]}# {alertMessage}";
                 string description = $"Alert details:{Environment.NewLine}{requestBody}";
-                WorkItem workItem = await CreateAdoTask("DevDiv\\NET Tools\\install-scripts-incidents", title, description).ConfigureAwait(false);
+
+
+                bool workItemExists = await ActiveWorkItemExists(workItemClient, devdivProject, title, IncidentAreaPath)
+                    .ConfigureAwait(false);
+
+                if (workItemExists)
+                {
+                    log.LogInformation("There is already a work item tracking this issue. A new work item will not be created.");
+                    continue;
+                }
+
+                WorkItem workItem = await CreateAdoTask(workItemClient, devdivProject, IncidentAreaPath, title, description).ConfigureAwait(false);
 
                 string workItemUrl = (workItem.Links?.Links?["html"] as ReferenceLink)?.Href ?? "<url-not-found>";
                 string successMessage = $"Work item with ID {workItem.Id} was created at address {workItemUrl}";
-            
+
                 log.LogInformation(successMessage);
                 createdWorkItems.Add(workItem);
             }
@@ -96,34 +105,57 @@ namespace MonitoringFunctions.Functions
         }
 
         /// <summary>
+        /// Searches for an active work item in the given project with the given title.
+        /// </summary>
+        /// <param name="project">Project to search for the work item.</param>
+        /// <param name="title">Title of the work item.</param>
+        /// <param name="areaPath">Area path of the work item.</param>
+        /// <param name="cancellationToken">Token to cancel the operation.</param>
+        /// <returns>True if a work item is found. False otherwise.</returns>
+        private static async Task<bool> ActiveWorkItemExists(
+            WorkItemTrackingHttpClient workItemClient,
+            TeamProject project,
+            string title,
+            string areaPath,
+            CancellationToken cancellationToken = default)
+        {
+            string wiqlQuery = $@"SELECT
+                    [System.Id]
+                FROM workitems
+                WHERE
+                    [System.Title] = '{title.Replace('\'', '_')}'
+                    AND [System.AreaPath] = '{areaPath.Replace('\'', '_')}'
+                    AND NOT [System.State] IN ('6 - Closed', 'Closed', 'Resolved', 'Cut', 'Completed')";
+
+            WorkItemQueryResult queryResult = await workItemClient.QueryByWiqlAsync(new Wiql() { Query = wiqlQuery }, project.Id,
+                null, null, null, cancellationToken).ConfigureAwait(false);
+
+            return queryResult.WorkItems.FirstOrDefault() != null;
+        }
+
+        /// <summary>
         /// Creates a bug in ADO with given fields.
         /// </summary>
-        /// <param name="title">Title of the work item.</param>
+        /// <param name="workItemClient">Client to use for creating the work item.</param>
+        /// <param name="project">ADO project to create the work item in</param>
         /// <param name="areaPath">AreaPath of the work item.</param>
+        /// <param name="title">Title of the work item.</param>
         /// <param name="description">Description of the work item.</param>
-        /// <param name="reproSteps">Reproduction steps of the work item. For work items with type "Bug", this field 
-        /// shows up in ADO instead of <paramref name="description"/>.</param>
         /// <param name="tags">Comma separated tags of the work item.</param>
+        /// <param name="cancellationToken">Token to cancel the operation.</param>
         /// <returns>The created work item</returns>
         private static async Task<WorkItem> CreateAdoTask(
+            WorkItemTrackingHttpClient workItemClient,
+            TeamProject project,
             string areaPath,
             string title,
             string? description = null,
             string? tags = null,
             CancellationToken cancellationToken = default)
         {
-            VssCredentials creds = new VssBasicCredential("", _devdivAdoPAT);
-            ProjectHttpClient projectClient = new ProjectHttpClient(_devdivCollectionUri, creds);
-            TeamProject devdivProject = await projectClient.GetProject(ProjectName).ConfigureAwait(false);
 
-            if (devdivProject == null)
-            {
-                throw new ProjectDoesNotExistException($"The project \"{ProjectName}\" was not found or you do not have permission to access it.");
-            }
-
-            WorkItemTrackingHttpClient workItemClient = new WorkItemTrackingHttpClient(_devdivCollectionUri, creds);
             JsonPatchDocument workItemJson = GetJsonForNewWorkItem(areaPath, title, description, tags);
-            WorkItem generatedBug = await workItemClient.CreateWorkItemAsync(workItemJson, devdivProject.Id, "Task",
+            WorkItem generatedBug = await workItemClient.CreateWorkItemAsync(workItemJson, project.Id, "Task",
                 null, null, null, null, null, cancellationToken).ConfigureAwait(false);
 
             if (generatedBug == null)
@@ -137,11 +169,9 @@ namespace MonitoringFunctions.Functions
         /// <summary>
         /// Creates a <see cref="JsonPatchDocument"/> for creating a work item with the given fields.
         /// </summary>
-        /// <param name="title">Title of the work item.</param>
         /// <param name="areaPath">AreaPath of the work item.</param>
+        /// <param name="title">Title of the work item.</param>
         /// <param name="description">Description of the work item.</param>
-        /// <param name="reproSteps">Reproduction steps of the work item. For work items with type "Bug", this field 
-        /// shows up in ADO instead of <paramref name="description"/>.</param>
         /// <param name="tags">Comma separated tags of the work item.</param>
         /// <returns><see cref="JsonPatchDocument"/> that defines a work item.</returns>
         private static JsonPatchDocument GetJsonForNewWorkItem(
