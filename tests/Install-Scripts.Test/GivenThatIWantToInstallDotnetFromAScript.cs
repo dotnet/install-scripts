0aa4951a546b42c6a4ff5ed553065ddf0a494098
Matt Galbraith <mattgal@microsoft.com>
2021-11-12 16:05:41 -0800

    Also update current

--- tests/Install-Scripts.Test/GivenThatIWantToInstallDotnetFromAScript.cs
+++ tests/Install-Scripts.Test/GivenThatIWantToInstallDotnetFromAScript.cs
@@ -30,7 +30,7 @@ namespace Microsoft.DotNet.InstallationScript.Tests
                 ("5.0", "5\\.0\\..*", Quality.None),
                 ("6.0", "6\\.0\\..*", Quality.Daily),
                 ("6.0", "6\\.0\\..*", Quality.None),
-                ("Current", "5\\.0\\..*", Quality.None),
+                ("Current", "6\\.0\\..*", Quality.None),
                 ("LTS", "6\\.0\\..*", Quality.None),
             };
 
