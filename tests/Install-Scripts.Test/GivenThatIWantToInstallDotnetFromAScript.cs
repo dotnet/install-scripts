bc4fc1420740edd8b2cbd7030c80c8a5740d98a6
Yuliia Kovalova <ykovalova@microsoft.com>
2022-08-01 17:06:28 +0200

    undo extra changes

--- tests/Install-Scripts.Test/GivenThatIWantToInstallDotnetFromAScript.cs
+++ tests/Install-Scripts.Test/GivenThatIWantToInstallDotnetFromAScript.cs
@@ -64,7 +64,7 @@ namespace Microsoft.DotNet.InstallationScript.Tests
                 ("release/2.1.8xx", "2\\.1\\.8.*", Quality.None),
                 ("release/2.2.4xx", "2\\.2\\.4.*", Quality.None),
                 ("release/3.0.1xx", "3\\.0\\.1.*", Quality.None),
-                //("release/3.1.4xx", "3\\.1\\.4.*", Quality.None),
+               // ("release/3.1.4xx", "3\\.1\\.4.*", Quality.None),
                 ("release/5.0.1xx", "5\\.0\\.1.*", Quality.None),
                 ("release/5.0.2xx", "5\\.0\\.2.*", Quality.None),
                 // Branches are no longer supported starting 6.0, but there are channels that correspond to branches.
