adc9b118c45f5a3de10076002131066701c00a91
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2023-09-22 15:23:21 +0200

    fix /usr/bin/stat placement

--- src/dotnet-install.sh
+++ src/dotnet-install.sh
@@ -567,9 +567,9 @@ validate_remote_local_file_sizes()
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         # hardcode in order to avoid conflicts with GNU stat
-        file_size="$(/usr/bin/stat -c '%s' "$downloaded_file")"
+        file_size="$(stat -c '%s' "$downloaded_file")"
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-        file_size="$(stat -f '%z' "$downloaded_file")"
+        file_size="$(/usr/bin/stat -f '%z' "$downloaded_file")"
     fi  
     
     if [ -n "$file_size" ]; then
