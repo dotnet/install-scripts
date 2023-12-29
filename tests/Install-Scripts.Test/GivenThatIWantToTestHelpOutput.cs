03812d7ffae2fef711fe701cbd441b53d8a640e1
Chet Husk <baronfel@users.noreply.github.com>
2023-12-29 01:39:17 -0600

    Add usage examples of the two primary modes of invoking this script (#409)

---
+++ tests/Install-Scripts.Test/GivenThatIWantToTestHelpOutput.cs
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using VerifyTests;
+using Xunit;
+using Microsoft.NET.TestFramework.Assertions;
+
+namespace Microsoft.DotNet.InstallationScript.Tests;
+
+public class GivenThatIWantToTestHelpOutput : TestBase
+{
+    public GivenThatIWantToTestHelpOutput(VerifySettings settings = null)
+            : base(settings) { }
+
+    [Fact]
+    public void InvokingHelpTriggersHelpForPowershell()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            // Testing powershell output is intended for Windows
+            return;
+        }
+
+        var commandResult = CreateInstallCommand(new List<string> { "-help" })
+                        .CaptureStdOut()
+                        .CaptureStdErr()
+                        .Execute();
+        
+        commandResult.Should().Pass();
+        // Verify is not used since the $PSCommandPath could differ from machine to machine, hence using explicit verification of the examples from the script file
+        commandResult.Should().HaveStdOutContaining("dotnet-install.ps1 -Version 7.0.401");
+        commandResult.Should().HaveStdOutContaining("Installs the .NET SDK version 7.0.401");
+        commandResult.Should().HaveStdOutContaining("dotnet-install.ps1 -Channel 8.0 -Quality GA");
+        commandResult.Should().HaveStdOutContaining("Installs the latest GA (general availability) version of the .NET 8.0 SDK");
+        
+    }
+
+    [Fact]
+    public void InvokingHelpTriggersHelpForBash()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            // Testing bash output is intended for non-Windows
+            return;
+        }
+
+        var commandResult = CreateInstallCommand(new List<string> { "--help" })
+                        .CaptureStdOut()
+                        .CaptureStdErr()
+                        .Execute();
+        
+        commandResult.Should().Pass();
+        // Verify is not used since the location could differ from machine to machine, hence using explicit verification of the help output
+        commandResult.Should().HaveStdOutContaining("Install a .NET SDK of a given Quality from a given Channel");
+        commandResult.Should().HaveStdOutContaining("dotnet-install.sh [-c|--channel <CHANNEL>] [-q|--quality <QUALITY>]");
+        commandResult.Should().HaveStdOutContaining("# Install a .NET SDK of a specific public version");
+        commandResult.Should().HaveStdOutContaining("dotnet-install.sh [-v|--version <VERSION>]");
+        commandResult.Should().HaveStdOutContaining("dotnet-install.sh -h|-?|--help");
+    }
+}
